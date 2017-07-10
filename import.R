b9073257f8bec3306543dcd6e8873c301e5504a8
Travis Gerke <tgerke@hsph.harvard.edu>
2017-07-10 15:11:29 -0400

    clean up columns

--- import.R
+++ import.R
@@ -1,7 +1,7 @@
 #######################################################################################
 # options, libraries
 
-setwd("~/Dropbox/projects/ebiGWASplot")
+setwd("~/Dropbox/projects/ebiGWASplot/gwasSNPs")
 
 options(stringsAsFactors=FALSE)
 
@@ -9,5 +9,17 @@ library(readr)
 library(dplyr)
 
 #####################################################################################
+# downloaded from http://www.ebi.ac.uk/gwas/ with search "cancer"
 
 dat <- read_tsv("data/gwas-association-downloaded_2017-07-10-cancer.tsv")
+head(dat)
+
+# let's subset down a little and clean up names
+names(dat)
+dat <- dat %>% select(Cancer=`DISEASE/TRAIT`, Region=REGION, Chr=CHR_ID,
+               Pos=CHR_POS, Context=CONTEXT, Intergenic=INTERGENIC, 
+               RAF=`RISK ALLELE FREQUENCY`, OR=`OR or BETA`)
+
+#####################################################################################
+# 
+
